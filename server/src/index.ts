132b65c523529c90a393a2f2c9feb6aabedc08b6
Firas YAHYAOUI <fyrasdjabou.el19@gmail.com>
2022-07-08 22:11:02 +0100

    Add API test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,7 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import { graphqlHTTP } from "express-graphql";
+import app from "./app";
 import connectDB from "./config/db";
-import schema from "./schema/schema";
 
 const port = process.env.PORT || 5000;
 
-dotenv.config();
-const app = express();
 connectDB();
-app.use(cors());
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
-
 app.listen(port, () => console.log(`Server started on port ${port}`));
